e852bd7bde625a0ccbf81764d03c81c6ce172367
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2024-12-03 16:20:38 -0500

    Update day2a.py

--- day2/day2a.py
+++ day2/day2a.py
@@ -1,1 +1,42 @@
+class AOC2aSol:
+    def __init__(self, p):
+        self.path = p
+        self.sol = 0
+        with open(self.path,'r') as f:
+            
+            csv_reader = csv.reader(f)
+            for line in csv_reader:
+                self.sol += self.evallist(list(map(int,line[0].split(' '))))
+                
+    def getsol(self):
+        return self.sol
+        
+                
+    def evallist(self,lista):
+        incr = False
+        decr = False
+        for a in range(1, len(lista)):
+            temp = lista[a] - lista[a-1]
+            if abs(temp) > 3:
+                return 0
+            if abs(temp) == 0:
+                return 0
+            if temp > 0:
+                if decr == False:
+                    if incr == False:
+                        incr = True
+                else:
+                    return 0
+            if temp < 0:
+                if incr == False:
+                    if decr == False:
+                        decr = True
+                else:
+                    return 0
+                
+        return 1
+        
+            
+        
+                
 
