ac167158b3257fbb30e7b2c15e9fc63582eba0b8
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2024-12-03 14:23:24 -0500

    Update day1a.py
    ????

--- day1/day1a.py
+++ day1/day1a.py
@@ -1,1 +1,22 @@
-
+class AOC1aSol:
+    def __init__(self, p):
+        self.path = p
+        self.list1 = []
+        self.list2 = []
+    
+        with open(self.path,'r') as f:
+            csv_reader = csv.reader(f)
+            for line in csv_reader:
+                rr = re.findall(r'(\d{5})',line[0])
+                if rr:
+                    self.list1.append(int(rr[0]))
+                    self.list2.append(int(rr[1]))
+        
+    def getsol(self):
+        accrue = 0
+        self.list1.sort()
+        self.list2.sort()
+        for l in range(len(self.list1)):
+            accrue += abs(self.list1[l]-self.list2[l])
+        
+        return accrue
