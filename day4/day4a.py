cdfa2c87d142fba52e7cfcc6028615edc2b760e4
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2024-12-04 23:52:13 -0500

    Update day4a.py

--- day4/day4a.py
+++ day4/day4a.py
@@ -4,66 +4,61 @@ class AOC4aSol:
         self.countxmas = 0
         self.dest = []
     
-        with open(self.path,'r') as f:
+        
+        with open(self.path, 'r') as f:
             for l in f.readlines():
                 self.dest.append(l.strip('\n'))
     
-        self.maxi = len(self.dest[0])
-        self.maxj = len(self.dest)
-        
-        
-     
+        self.maxi = len(self.dest)  
+        self.maxj = len(self.dest[0])  
 
     def check_position(self, i, j):
+       
         directions = {
-            "horizontal": [(0, -1), (0, 1)],
-            "vertical": [(-1, 0), (1, 0)],
-            "right_diagonal": [(-1, 1), (1, -1)],
-            "left_diagonal": [(-1, -1), (1, 1)]
+            "horizontal": [(0, 1), (0, -1)],  
+            "vertical": [(1, 0), (-1, 0)],  
+            "right_diagonal": [(1, 1), (-1, -1)],  
+            "left_diagonal": [(1, -1), (-1, 1)]   
         }
 
-        ret = {k: [(i, j)] for k in directions}  
-
-        for n in range(1, 5):  
-            for k, v in directions.items():
-                for r, c in v:
-                    ni, nj = i + r * n, j + c * n
-                    if 0 <= ni < self.maxi and 0 <= nj < self.maxj:
-                        ret[k].append((ni, nj))
-
-        self.makestring(self.valid_path(ret))
-        
+        for k, v in directions.items():
+            for r, c in v:
+                substring = self.extract_string(i, j, r, c)
+                if substring:
+                    self.countxmas += self.count_occurrences(substring, "XMAS")
         return None
 
-    
-    def valid_path(self,arcs):
-        sorte = {}
-    
-        for g, p in arcs.items():
-            sorte[g] = sorted(p, key=lambda x: (x[0], x[1]))
-            
-        return sorte
-    
-    def makestring(self,sortedarcs):
-        for k,v in sortedarcs.items():
-            temp = ''.join(self.dest[vs[0]][vs[1]] for vs in v)
-            if 'XMAS' in temp:
-                self.countxmas += 1
-            if 'SAMX' in temp:
-                self.countxmas += 1
-        return None
-            
-            
+    def extract_string(self, i, j, dr, dc):
+        """
+        Extract a string from the grid starting at (i, j) in direction (dr, dc).
+        """
+        chars = []
+        for n in range(4):  #extract up to 4 characters
+            ni, nj = i + dr * n, j + dc * n
+            if 0 <= ni < self.maxi and 0 <= nj < self.maxj:
+                chars.append(self.dest[ni][nj])
+            else:
+                return None  #stop if out of bounds
+        return ''.join(chars)
 
-    
+    @staticmethod
+    def count_occurrences(string, substring):
+        """
+        Counts all occurrences of a substring in a string, including overlaps.
+        """
+        count = 0
+        start = 0
+        while True:
+            start = string.find(substring, start)
+            if start == -1:  #no more occurrences
+                break
+            count += 1
+            start += 1  #move one step forward to allow overlaps
+        return count
+
     def getsol(self):
-        for r in range(0,self.maxi):
-            for c in range(0,self.maxj):
-                self.check_position(r,c)
-        
+        #check all grid positions
+        for r in range(self.maxi):
+            for c in range(self.maxj):
+                self.check_position(r, c)
         return self.countxmas
-                
-                
-    
-        
-    
